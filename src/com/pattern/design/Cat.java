8cac6c981d2d364ac59d82f6d44ed92f1c707be6
Marlon Monzon <martianworm17@gmail.com>
2018-08-19 00:11:59 +0800

    Initial commit

---
+++ src/com/pattern/design/Cat.java
@@ -0,0 +1,10 @@
+package com.pattern.design;
+
+public class Cat extends Animal {
+
+    public Cat() {
+        super();
+
+        setSound("Meow");
+    }
+}
---
+++ src/com/pattern/design/Dog.java
@@ -0,0 +1,14 @@
+package com.pattern.design;
+
+public class Dog extends Animal {
+
+    public void digHole() {
+        System.out.println("Dug a hole");
+    }
+
+    public Dog() {
+        super();
+
+        setSound("Bark");
+    }
+}
